0e22ca12552fa4a97d04454f396c8e1b8d80e668
miguel-oliveiraa <miguellgomes64@gmail.com>
2022-11-18 18:43:47 -0300

    first commit

---
+++ src/pages/login/index.js
@@ -0,0 +1,16 @@
+import react from "react";
+import Logo from "../../assets/reclama.png";
+
+export default function Login() {
+  return (
+    <div>
+      <div>
+        <img src={Logo} alt="reclama" />
+      </div>
+      <div>
+        <p>texto</p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
